ebd20c66330bf850a1e7af1c3cc954224c2b69b5
nfrank5 <nfrankenfeld@gmail.com>
2023-03-27 16:39:59 +0800

    Everytingh kind of working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,16 +5,15 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking_params)
-
+    @booking = Booking.create(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   private
-
-
   def booking_params
     params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email, :id])
   end
