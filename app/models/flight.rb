b56948a9dd368125846065009188bfb1bbf385eb
nfrank5 <nfrankenfeld@gmail.com>
2023-03-27 11:40:52 +0800

    Add steps 1, 2, and 3 of Passenger information

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,16 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
-
-  def self.searchxxx(search_params)
-    dep_arr = Flight.all.where("departure_airport_id = ? AND arrival_airport_id = ? " ,
-      search_params[:departure_airport_id],
-      search_params[:arrival_airport_id],
-    )
-    dep_arr.select do |f|
-      "#{f.start_date.year}/#{f.start_date.month}/#{f.start_date.day}" == search_params[:flight_date]
-    end
-  end
+  has_many :bookings
 
   def self.search(search_params)
     Flight.where(departure_airport_id: search_params[:departure_airport_id],
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flights do
     collection do
