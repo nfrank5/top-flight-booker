0d3972406bff74bbe09379072a2816d47150d2ca
nfrank5 <nfrankenfeld@gmail.com>
2023-03-27 13:29:03 +0800

    Add steps 5 and 6 of passenger information

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,22 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    params[:passangers_qty].to_i.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
   end
 
   def show
   end
+
+  private
+
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email, :id])
+  end
+
 end
